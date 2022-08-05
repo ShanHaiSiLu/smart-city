ecc25b1e3a128adaf1b5f1f75e7ab3e542031384
许晶俞 <shanhaisilu@foxmail.com>
2022-08-05 14:24:59 +0800

    添加天空盒和抗锯齿效果

--- src/three/renderer.js
+++ src/three/renderer.js
@@ -1,7 +1,10 @@
 import * as THREE from "three";
 
 // 渲染器
-let renderer = new THREE.WebGLRenderer();
+let renderer = new THREE.WebGLRenderer({
+  // 抗锯齿
+  antialias: true,
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 
--- src/three/scene.js
+++ src/three/scene.js
@@ -3,4 +3,19 @@ import * as THREE from "three";
 // 场景
 let scene = new THREE.Scene();
 
+// 添加天空盒
+const skyBoxLoader = new THREE.CubeTextureLoader().setPath("./skyBox/");
+
+const skyBox = skyBoxLoader.load([
+  "1.jpg",
+  "2.jpg",
+  "3.jpg",
+  "4.jpg",
+  "5.jpg",
+  "6.jpg",
+]);
+
+scene.background = skyBox;
+scene.environment = skyBox;
+
 export default scene;
